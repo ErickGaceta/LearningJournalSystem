14f197be1f99e003eb02efe639ec99585d371a2e
Erick Gaceta <gacetaerick124@gmail.com>
2026-01-27 11:12:51 +0800

    Minor Fix

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -3,7 +3,7 @@
         @if(session('success'))
         <div class="bg-green-500/10 border border-green-500/20 text-green-400 px-4 py-3 rounded-xl text-sm">
             <div class="flex items-center">
-                <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
+                <svg class="w-3 h-3 mr-2" fill="currentColor" viewBox="0 0 20 20">
                     <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                 </svg>
                 {{ session('success') }}
