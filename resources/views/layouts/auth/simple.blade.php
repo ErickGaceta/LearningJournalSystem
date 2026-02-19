ca46341cfae510305b09c90aad80c0fa92474a8e
Erick Gaceta <gacetaerick124@gmail.com>
2026-02-19 11:26:59 +0800

    Stylized the Auth template

--- resources/views/layouts/auth/simple.blade.php
+++ resources/views/layouts/auth/simple.blade.php
@@ -5,7 +5,7 @@
     </head>
     <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
         <div id="animated-login" class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-            <div class="flex w-full max-w-sm flex-col gap-2">
+            <div class="flex w-full max-w-sm flex-col gap-2 border p-10 rounded-md" style="backdrop-filter: invert(25%); border: 1px solid #737373; box-shadow: 1px -1px 19px 11px rgba(0,0,0,0.59);">
                 <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
                     <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-md">
                         <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
